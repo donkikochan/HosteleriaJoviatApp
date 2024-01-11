143a348f4f60ad59b608e3c4503142e1a1463663
Tulio Toledo Bigon <tuliontoledobigon2@gmail.com>
2024-01-11 11:47:36 +0100

    List the users or chefList

---
+++ HosteleriaApp/Components/ChefList/ItemsChef.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+
+const Items = ({ name }) => {
+  const [isPressed, setIsPressed] = useState(false);
+
+  return (
+    <TouchableOpacity
+      onPressIn={() => setIsPressed(true)}  
+      onPressOut={() => setIsPressed(false)}  
+      style={[styles.itemContainer, isPressed && styles.itemPressed]}
+    >
+      <Image
+        source={require('./ChefList/tulio.jpg')}
+        style={styles.userIcon}
+      />
+      <Text style={styles.itemText}>{name}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+   
+    backgroundColor: '#A9A9A9',
+    borderRadius: 20,
+    paddingVertical: 4,
+    paddingHorizontal: 10,
+    marginVertical: 6,
+    width: '100%',
+   
+  },
+  itemPressed: {
+    backgroundColor: 'black', 
+  },
+  itemText: {
+    fontSize: 20,
+    color: 'white',
+    marginLeft: 13, 
+  },
+  userIcon: {
+    width: 40,
+    height: 40,
+    borderRadius: 25,
+  },
+  
+});
+
+export default Items;
+
+
