f329c3e49d8f9259e73cd11caf9c8f7b02377ff5
María José Issa <95626214+Majoissa@users.noreply.github.com>
2024-01-29 13:25:49 +0100

    SP1-T013-Restaurants screen component (1) work with tulio

--- HosteleriaApp/Components/RestaurantInfoCard/RestaurantInfoCard.js
+++ HosteleriaApp/Components/RestaurantInfoCard/RestaurantInfoCard.js
@@ -1,104 +1,107 @@
 import React from "react";
-import {StyleSheet, View, Text, TouchableOpacity} from "react-native";
-import {Ionicons} from "@expo/vector-icons";
-import {useNavigation} from "@react-navigation/native";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
-function RestaurantInfoCard({titol, descripcio, accio, sourceImage, screen}) {
+function RestaurantInfoCard({ titol, descripcio, accio, sourceImage, screen }) {
   const navigation = useNavigation();
-    return (
-        <View style={styles.container}>
-            <View style={styles.group2}>
-                <View style={styles.group}>
-                    <View style={styles.group4}>
-                        <Text style={styles.nomRestaurant}>{titol}</Text>
-                        <View style={styles.descripcioRestaurantRow}>
-                            <Text style={styles.descripcioRestaurant}>{descripcio}</Text>
-                            <View style={styles.descripcioRestaurantFiller}></View>
-                            <TouchableOpacity style={styles.group3} onPress={() => navigation.navigate(screen)}>
-                                <Ionicons
-                                    name={sourceImage}
-                                    style={styles.iconaImatge}
-                                    size={40}
-                                    resizeMode="cover"
-                                ></Ionicons>
-                                <Text style={styles.clickHere}>{accio}</Text>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                </View>
+  return (
+    <View style={styles.container}>
+      <View style={styles.group2}>
+        <View style={styles.group}>
+          <View style={styles.group4}>
+            <Text style={styles.nomRestaurant}>{titol}</Text>
+            <View style={styles.descripcioRestaurantRow}>
+              <Text style={styles.descripcioRestaurant}>{descripcio}</Text>
+              <View style={styles.descripcioRestaurantFiller}></View>
+              <TouchableOpacity
+                style={styles.group3}
+                onPress={() => navigation.navigate(screen)}
+              >
+                <Ionicons
+                  name={sourceImage}
+                  style={styles.iconaImatge}
+                  size={40}
+                  resizeMode="cover"
+                ></Ionicons>
+                <Text style={styles.clickHere}>{accio}</Text>
+              </TouchableOpacity>
             </View>
+          </View>
         </View>
-    );
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        overflow: "visible",
-        backgroundColor: "rgba(230, 230, 230,0)",
-    },
-    group2: {
-        overflow: "visible",
-        borderRadius: 9,
-        borderWidth: 0,
-        borderColor: "#000000",
-        backgroundColor: "#E6E6E6",
-        justifyContent: "center",
-        paddingBottom: 10,
-    },
-    group: {
-        width: 360,
-        flexDirection: "row",
-        overflow: "hidden",
-        justifyContent: "center",
-        alignSelf: "center",
-    },
-    group4: {
-        width: 248,
-        alignSelf: "center",
-        paddingTop: 10,
-    },
-    nomRestaurant: {
-        color: "rgba(0,0,0,1)",
-        fontSize: 20,
-        textAlign: "center",
-        width: 224,
-        marginLeft: 12,
-        fontWeight: "bold",
-    },
-    descripcioRestaurant: {
-        color: "rgba(0,0,0,1)",
-        fontSize: 15,
-        textAlign: "center",
-        width: 200,
-        marginRight: 50,
-    },
-    descripcioRestaurantFiller: {
-        flex: 1,
-        flexDirection: "row",
-    },
-    group3: {
-        width: 91,
-        alignItems: "flex-end",
-        justifyContent: "center",
-    },
-    iconaImatge: {
-        width: 40,
-        height: 40,
-        marginRight: 26,
-    },
-    clickHere: {
-        color: "#121212",
-        textAlign: "center",
-        fontSize: 18,
-        width: 91,
-        height: 25,
-        fontWeight: "bold",
-    },
-    descripcioRestaurantRow: {
-        flexDirection: "row",
-        marginTop: 11,
-        alignSelf: "center",
-    },
+  container: {
+    overflow: "visible",
+    backgroundColor: "rgba(230, 230, 230,0)",
+  },
+  group2: {
+    overflow: "visible",
+    borderRadius: 9,
+    borderWidth: 0,
+    borderColor: "#000000",
+    backgroundColor: "#E6E6E6",
+    justifyContent: "center",
+    paddingBottom: 10,
+  },
+  group: {
+    width: 360,
+    flexDirection: "row",
+    overflow: "hidden",
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+  group4: {
+    width: 248,
+    alignSelf: "center",
+    paddingTop: 10,
+  },
+  nomRestaurant: {
+    color: "rgba(0,0,0,1)",
+    fontSize: 20,
+    textAlign: "center",
+    width: 224,
+    marginLeft: 12,
+    fontWeight: "bold",
+  },
+  descripcioRestaurant: {
+    color: "rgba(0,0,0,1)",
+    fontSize: 15,
+    textAlign: "center",
+    width: 200,
+    marginRight: 50,
+  },
+  descripcioRestaurantFiller: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  group3: {
+    width: 91,
+    alignItems: "flex-end",
+    justifyContent: "center",
+  },
+  iconaImatge: {
+    width: 40,
+    height: 40,
+    marginRight: 26,
+  },
+  clickHere: {
+    color: "#121212",
+    textAlign: "center",
+    fontSize: 18,
+    width: 91,
+    height: 25,
+    fontWeight: "bold",
+  },
+  descripcioRestaurantRow: {
+    flexDirection: "row",
+    marginTop: 11,
+    alignSelf: "center",
+  },
 });
 
 export default RestaurantInfoCard;
--- HosteleriaApp/Components/RestaurantScreen/RestaurantScreen.js
+++ HosteleriaApp/Components/RestaurantScreen/RestaurantScreen.js
@@ -1,12 +1,25 @@
+import React from "react";
 import { StyleSheet, View } from "react-native";
 import Navbar from "../Navbar/Navbar";
 import FooterNavbar from "../FooterNavbar/FooterNavbar";
 import CarouselDef from "../Carousel/CaroselDef";
+import Items from "../ChefList/ItemsChef";
+import RestaurantInfoCard from "../RestaurantInfoCard/RestaurantInfoCard";
 
-function RestaurantScreen({route}) {
-  const id = route?.params?.id; 
+function RestaurantScreen({ route }) {
+  const id = route?.params?.id;
+
+  const itemsData = ["Item 1", "Item 2", "Item 3"];
+  const restaurantInfo = {
+    titol: "Nombre del Restaurante",
+    descripcio: "Descripción del Restaurante",
+    accio: "Ver Más",
+    sourceImage: "ios-restaurant",
+    screen: "DetalleRestaurante",
+  };
+
   return (
-    <View>
+    <View style={styles.container}>
       <Navbar
         showGoBack={true}
         showLogIn={false}
@@ -14,9 +27,22 @@ function RestaurantScreen({route}) {
         text="Login"
       />
       <CarouselDef />
+
+      <RestaurantInfoCard {...restaurantInfo} />
+
+      {itemsData.map((name, index) => (
+        <Items key={index} name={name} />
+      ))}
+
       <FooterNavbar />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default RestaurantScreen;
