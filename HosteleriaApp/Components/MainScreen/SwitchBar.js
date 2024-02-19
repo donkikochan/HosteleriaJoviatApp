0167bb0cbec20ad58607f5c39fc8ae836f2b3ddf
María José Issa <95626214+Majoissa@users.noreply.github.com>
2024-02-19 10:53:14 +0100

    SP2-T004-Customize Switch Design

--- HosteleriaApp/Components/MainScreen/SwitchBar.js
+++ HosteleriaApp/Components/MainScreen/SwitchBar.js
@@ -1,27 +1,83 @@
-// SwitchBar.js
-import React from 'react';
-import { View, Text, Switch, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Animated,
+} from "react-native";
 
 const SwitchBar = ({ isMapView, onToggleView }) => {
+  const [backgroundPosition, setBackgroundPosition] = useState(
+    new Animated.Value(0)
+  );
+
+  const toggleSwitch = () => {
+    Animated.timing(backgroundPosition, {
+      toValue: isMapView ? 0 : 100,
+      duration: 250,
+      useNativeDriver: false,
+    }).start();
+
+    onToggleView(!isMapView);
+  };
+
+  const getTextStyle = (isActive) => ({
+    ...styles.switchText,
+    color: isActive ? "black" : "white",
+  });
+
+  const backgroundStyle = {
+    ...styles.switchBackground,
+    left: backgroundPosition,
+  };
+
   return (
     <View style={styles.switchContainer}>
-      <Text style={styles.switchText}>List</Text>
-      <Switch value={isMapView} onValueChange={onToggleView} />
-      <Text style={styles.switchText}>Map</Text>
+      <TouchableOpacity
+        style={styles.switchButton}
+        activeOpacity={1}
+        onPress={toggleSwitch}
+      >
+        <Animated.View style={backgroundStyle} />
+        <Text style={getTextStyle(!isMapView)}>List</Text>
+        <Text style={getTextStyle(isMapView)}>Map</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   switchContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 10,
+    marginVertical: 20,
+    width: 200,
+    height: 30,
+    borderRadius: 20,
+    backgroundColor: "black",
+    overflow: "hidden",
+  },
+  switchButton: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
   },
   switchText: {
-    marginHorizontal: 10,
-    fontSize: 16,
+    color: "#FFFFFF",
+    fontWeight: "bold",
+    textAlign: "center",
+    width: "50%",
+  },
+  switchBackground: {
+    position: "absolute",
+    width: "50%",
+    height: "100%",
+    borderRadius: 20,
+    backgroundColor: "#D9D9D9",
+    borderWidth: 2,
+    borderColor: "black",
+    zIndex: -1,
   },
 });
 
