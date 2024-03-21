a96751fdf7f9d5a62ed2d008248f2683aa7cfff8
TulioToledoB <tuliontoledobigon2@gmail.com>
2024-03-21 12:35:59 +0100

    SP2-T027-hideButtons-Login_logout_popUp

--- HosteleriaApp/Components/Navbar/Navbar.js
+++ HosteleriaApp/Components/Navbar/Navbar.js
@@ -1,52 +1,57 @@
 import React from "react";
-import {Dimensions} from "react-native";
-import {View, Text, TouchableOpacity, StyleSheet} from "react-native";
-import {Image} from "react-native";
+import { Dimensions } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { Image } from "react-native";
 import SearchBar from "./SearchBar";
 import GoBackArrow from "./GoBackArrow";
-import {useNavigation} from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
 
 const Navbar = (props) => {
-    const navigation = useNavigation();
-    return (
-        <View style={styles.navbar}>
-            {props.showGoBack && <GoBackArrow navigation={navigation}/>}
-            {props.showLogIn && (
-                <TouchableOpacity onPress={() => navigation.navigate(props.screen)}>
-                    <Text style={styles.text}>{props.text}</Text>
-                </TouchableOpacity>
-            )}
-            {props.showSearch && <SearchBar handleSearch= {props.handleSearch} />}
-            <TouchableOpacity onPress={() => navigation.navigate('Joviat')}>
-                <Image
-                    source={require("../../assets/logo.png")}
-                    style={{width: 50, height: 50}}
-                />
-            </TouchableOpacity>
-        </View>
-    );
+  const navigation = useNavigation();
+  return (
+    <View style={styles.navbar}>
+      {props.showGoBack && <GoBackArrow navigation={navigation} />}
+      {props.showLogIn && (
+        <TouchableOpacity onPress={() => navigation.navigate(props.screen)}>
+          <Text style={styles.text}>{props.text}</Text>
+        </TouchableOpacity>
+      )}
+      {props.showLogOut && (
+        <TouchableOpacity onPress={props.handleLogOut}>
+          <Text style={styles.text}>{props.text}</Text>
+        </TouchableOpacity>
+      )}
+      {props.showSearch && <SearchBar handleSearch={props.handleSearch} />}
+      <TouchableOpacity onPress={() => navigation.navigate("Joviat")}>
+        <Image
+          source={require("../../assets/logo.png")}
+          style={{ width: 50, height: 50 }}
+        />
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const screenWidth = Dimensions.get("window").width;
 const styles = StyleSheet.create({
-    navbar: {
-        color: "white",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        backgroundColor: "#111820",
-        paddingHorizontal: 10,
-        paddingVertical: 15,
-        position: "absolute",
-        paddingTop: 47,
-        top: 0,
-        width: screenWidth,
-    },
-    text: {
-        color: "white",
-        fontSize: 16,
-        marginLeft: 10,
-    },
+  navbar: {
+    color: "white",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#111820",
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+    position: "absolute",
+    paddingTop: 47,
+    top: 0,
+    width: screenWidth,
+  },
+  text: {
+    color: "white",
+    fontSize: 16,
+    marginLeft: 10,
+  },
 });
 
 export default Navbar;
