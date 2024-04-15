c3042eec438f908ad01316c930742cde50956df3
Victoria <victoriaeso904@gmail.com>
2024-04-15 11:36:35 +0200

    SP2-T029-Fix weird behavior on FavoriteScreen

--- HosteleriaApp/Components/FavoritesScreen/FavRestScreen.js
+++ HosteleriaApp/Components/FavoritesScreen/FavRestScreen.js
@@ -1,116 +1,133 @@
-import React, { useEffect, useState } from "react";
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  StyleSheet,
-  ScrollView, ActivityIndicator,
-} from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
+import React, {useEffect, useState} from "react";
+import {ActivityIndicator, ScrollView, StyleSheet, Text, View,} from "react-native";
+import {useIsFocused, useNavigation} from "@react-navigation/native";
 import FooterNavbar from "../FooterNavbar/FooterNavbar";
 import Navbar from "../Navbar/Navbar";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import ListViewComponent from "../MainScreen/ListView";
 
 function FavRestScreen() {
-  const [favoriteRestaurants, setFavoriteRestaurants] = useState([]);
-  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [activeContent, setActiveContent] = useState("Favorite");
-  const [loading, setLoading] = useState(true);
-  const navigation = useNavigation(); // Obtener el objeto de navegación utilizando useNavigation
+    const [favoriteRestaurants, setFavoriteRestaurants] = useState([]);
+    const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
+    const [activeContent, setActiveContent] = useState("Favorite");
+    const [loading, setLoading] = useState(true);
+    const navigation = useNavigation(); // Obtener el objeto de navegación utilizando useNavigation
+    const [lastVisitedFavorites, setLastVisitedFavorites] = useState(true);
+    const isFocused = useIsFocused();
 
-  useEffect(() => {
-    const getFavoriteRestaurants = async () => {
-      try {
-        const keys = await AsyncStorage.getAllKeys();
-        const favoriteRestaurantKeys = keys.filter((key) =>
-            key.startsWith("restaurant_")
+    useEffect(() => {
+        setFilteredRestaurants([]);
+        const getFavoriteRestaurants = async () => {
+            try {
+                const keys = await AsyncStorage.getAllKeys();
+                const favoriteRestaurantKeys = keys.filter((key) =>
+                    key.startsWith("restaurant_")
+                );
+                if (favoriteRestaurantKeys.length > 0) {
+                    const favoriteRestaurantData = await AsyncStorage.multiGet(
+                        favoriteRestaurantKeys
+                    );
+                    const favoriteRestaurants = favoriteRestaurantData.map(
+                        ([key, value]) => JSON.parse(value)
+                    );
+                    setFavoriteRestaurants(favoriteRestaurants);
+                    setFilteredRestaurants(favoriteRestaurants);
+                } else {
+                    console.log("No hay restaurantes favoritos almacenados.");
+                    setFavoriteRestaurants([]);
+                    setFilteredRestaurants([]);
+                }
+            } catch (error) {
+                console.error("Error al obtener los restaurantes favoritos", error);
+                setLoading(false);
+            } finally {
+                setLoading(false);
+            }
+        };
+        getFavoriteRestaurants();
+    }, [isFocused]);
+
+    const handleSearch = (query) => {
+        setSearchQuery(query);
+        const filtered = favoriteRestaurants.filter((restaurant) =>
+            restaurant.nom.toLowerCase().includes(query.toLowerCase())
         );
-        if (favoriteRestaurantKeys.length > 0) {
-          const favoriteRestaurantData = await AsyncStorage.multiGet(
-              favoriteRestaurantKeys
-          );
-          const favoriteRestaurants = favoriteRestaurantData.map(
-              ([key, value]) => JSON.parse(value)
-          );
-          setFavoriteRestaurants(favoriteRestaurants);
-          setFilteredRestaurants(favoriteRestaurants);
-        } else {
-          console.log("No hay restaurantes favoritos almacenados.");
-        }
-      } catch (error) {
-        console.error("Error al obtener los restaurantes favoritos", error);
-      } finally {
-        setLoading(false);
-      }
+        setFilteredRestaurants(filtered);
     };
-    getFavoriteRestaurants();
-  }, []);
+    useEffect(() => {
+        // Actualizar lastVisitedFavorites cuando la pantalla se enfoca
+        if (isFocused) {
+            return navigation.addListener('beforeRemove', () => {
+                setLastVisitedFavorites(true);
+            });
+        }
+    }, [isFocused, navigation]);
+    const renderContent = () => {
+        if (loading) {
+            return <ActivityIndicator animating={false}/>;
+        } else if (favoriteRestaurants.length < 1) {
+            return (
+                <Text style={styles.noRestaurantsText}>
+                    No n'hi ha restaurants favorits guardats
+                </Text>
+            );
+        } else {
+            if (isFocused)
+                return (
+                    <ListViewComponent data={filteredRestaurants}
+                                       navigation={navigation}
+                                       lastVisitedFavorites={lastVisitedFavorites}
+                                       updateLastVisitedFavorites={updateLastVisitedFavorites}/>
+                );
+        }
+    };
 
-  const handleSearch = (query) => {
-    setSearchQuery(query);
-    const filtered = favoriteRestaurants.filter((restaurant) =>
-        restaurant.nom.toLowerCase().includes(query.toLowerCase())
+    const updateLastVisitedFavorites = (value) => {
+        setLastVisitedFavorites(value);
+    };
+
+    return (
+        <View style={styles.container}>
+            <Navbar
+                showGoBack={true}
+                showLogIn={false}
+                showSearch={true}
+                specialBackButton={true}
+                lastVisitedFavorites={lastVisitedFavorites}
+                text="Login"
+                screen="Login"
+                handleSearch={handleSearch}
+            />
+            <ScrollView style={styles.contentContainer}>
+                {renderContent()}
+            </ScrollView>
+            <FooterNavbar setActiveContent={activeContent} navigation={navigation}/>
+        </View>
     );
-    setFilteredRestaurants(filtered);
-  };
-  const renderContent = () => {
-    if (loading) {
-      return <ActivityIndicator animating={false} />;
-    } else if (favoriteRestaurants.length < 1) {
-      return (
-          <Text style={styles.noRestaurantsText}>
-            No n'hi ha restaurants favorits guardats
-          </Text>
-      );
-    } else {
-      return (
-          <ListViewComponent data={filteredRestaurants} navigation={navigation} />
-      );
-    }
-  };
-
-  return (
-      <View style={styles.container}>
-        <Navbar
-            showGoBack={true}
-            showLogIn={false}
-            showSearch={true}
-            text="Login"
-            screen="Login"
-            handleSearch={handleSearch}
-        />
-        <ScrollView style={styles.contentContainer}>
-          {renderContent()}
-        </ScrollView>
-        <FooterNavbar setActiveContent={activeContent} navigation={navigation} />
-      </View>
-  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: 120,
-  },
-  contentContainer: {
-    flex: 1,
-  },
-  noRestaurantsText: {
-    alignSelf: 'center',
-    justifyContent: 'space-between',
-    textAlign: 'center',
-    position: 'relative',
-    alignItems: 'center',
-    display: 'flex',
-    marginTop: '50%',
-    verticalAlign: 'middle',
-    paddingVertical: '20%',
-    fontSize: 20
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        paddingTop: 120,
+    },
+    contentContainer: {
+        flex: 1,
+    },
+    noRestaurantsText: {
+        alignSelf: 'center',
+        justifyContent: 'space-between',
+        textAlign: 'center',
+        position: 'relative',
+        alignItems: 'center',
+        display: 'flex',
+        marginTop: '50%',
+        verticalAlign: 'middle',
+        paddingVertical: '20%',
+        fontSize: 20
+    },
 });
 
 export default FavRestScreen;
--- HosteleriaApp/Components/Navbar/GoBackArrow.js
+++ HosteleriaApp/Components/Navbar/GoBackArrow.js
@@ -1,15 +1,32 @@
-import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import React, {useState} from "react";
+import {View, Text, TouchableOpacity, StyleSheet} from "react-native";
+import {Ionicons} from "@expo/vector-icons";
 
-const GoBackArrow = ({navigation}) => {
-  return (
-    <View>
-      <TouchableOpacity onPress={() => navigation.goBack()}>
-        <Ionicons name="chevron-back" size={24} color="white" />
-      </TouchableOpacity>
-    </View>
-  );
+const GoBackArrow = ({navigation, specialBackButton, lastVisitedFavorites, isFavorite}) => {
+    const handleGoBack = () => {
+
+        if (specialBackButton && lastVisitedFavorites) {
+            navigation.goBack();
+
+        } else if (specialBackButton && !lastVisitedFavorites) {
+            navigation.navigate("Home");
+
+        } else {
+            navigation.goBack();
+        }
+
+        if (isFavorite) {
+            navigation.navigate("Favorite");
+        }
+    };
+
+    return (
+        <View>
+            <TouchableOpacity onPress={handleGoBack}>
+                <Ionicons name="chevron-back" size={24} color="white"/>
+            </TouchableOpacity>
+        </View>
+    );
 };
 
 export default GoBackArrow;
